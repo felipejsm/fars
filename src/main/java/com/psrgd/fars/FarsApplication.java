9c63ba747c2cafa1a64450748aacdbde97a7bb6e
felipejsm <felipejsm41@gmail.com>
2026-06-23 21:44:46 -0300

    first commit

---
+++ src/main/java/com/psrgd/fars/FarsApplication.java
@@ -0,0 +1,13 @@
+package com.psrgd.fars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarsApplication.class, args);
+	}
+
+}
